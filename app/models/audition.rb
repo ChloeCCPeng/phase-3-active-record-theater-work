208f1e90492fb3b0af1c8cdb3cec068b21d25ed8
Chloe <chloepeng331@icloud.com>
2022-03-21 19:26:51 -0400

    comments added

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -2,6 +2,7 @@
 class Audition < ActiveRecord::Base
     belongs_to :role
 
+    # will change the the hired attribute to true
     def call_back
         # bind ing.pry
         self.hired = true
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,23 +1,27 @@
 class Role < ActiveRecord::Base
     has_many :auditions
 
+    #returns an array of names from the actors associated with this role
     def actors
         self.auditions.map do |audition|
             audition.actor
         end
     end
     
+    #returns an array of locations from the auditions associated with this role
     def locations
         self.auditions.map do |audition|
             place.location
         end
     end
 
+    # returns the first instance of the audition that was hired for this role or returns a string 'no actor has been hired for this role'
     def lead
         find = self.auditions.find{|audition| audition.hired == true}
         find ? find : "no actor has been hired for this role"
     end
 
+    # returns the second instance of the audition that was hired for this role or returns a string 'no actor has been hired for understudy for this role'
     def understudy
         find = self.auditions.select{|audition| audition.hired == true}
         find.second ? find.second : "no actor has been hired for this role"
